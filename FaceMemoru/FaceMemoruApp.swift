6e3297ad4acc1ace7921ce8d0c6bfe337f16f32c
Dias Manap <diasmanap1@gmail.com>
2022-02-08 22:00:39 +0600

    Initial Commit

---
+++ FaceMemoru/FaceMemoruApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMemoruApp.swift
+//  FaceMemoru
+//
+//  Created by Dias Manap on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceMemoruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
